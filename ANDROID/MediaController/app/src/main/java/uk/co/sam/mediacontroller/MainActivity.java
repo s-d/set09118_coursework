6ad1d7c2133b666797f6cc8ea7b4d44b3b504d28
Sam <samdixon05@gmail.com>
2016-03-22 22:04:11 +0000

    Added very basic bluetooth checking

--- ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/MainActivity.java
+++ ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/MainActivity.java
@@ -1,15 +1,20 @@
 package uk.co.sam.mediacontroller;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.bluetooth.BluetoothAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    public BluetoothHandler bHandler;
+    private BluetoothAdapter mBluetoothAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
     @Override
@@ -35,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         switch (id) {
             //noinspection SimplifiableIfStatement
             case R.id.action_connect:
+                if (mBluetoothAdapter == null) {
+                    Snackbar.make(this.findViewById(R.id.action_connect), "No Bluetooth for you.", Snackbar.LENGTH_LONG).show();
+                } else {
+                    Snackbar.make(this.findViewById(R.id.action_connect), "Bluetooth, son!", Snackbar.LENGTH_LONG).show();
+                }
                 return true;
             case R.id.action_disconnect:
                 return true;
