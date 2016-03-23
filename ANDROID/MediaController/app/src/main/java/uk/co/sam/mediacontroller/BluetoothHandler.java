caa51ea95707585b4807ace8ccdbcc2a73966713
Sam <samdixon05@gmail.com>
2016-03-23 15:46:59 +0000

    App checks if Bluetooth is available. App requests to turn on Bluetooth. App checks if Bluetooth was activated successfully. App shows a sample dialog alert.

--- ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/BluetoothHandler.java
+++ ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/BluetoothHandler.java
@@ -1,17 +1,23 @@
 package uk.co.sam.mediacontroller;
 
 import android.app.Activity;
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 
 /**
  * Created by Sam Dixon on 22/03/2016.
  */
 public class BluetoothHandler {
-    private BluetoothHandler bluetoothHandler;
+
+    private static final int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter bluetoothAdapter;
     private View view;
     private Activity activity;
@@ -22,9 +28,49 @@ public class BluetoothHandler {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
             Snackbar.make(this.view, "Bluetooth unavailable on this device.", Snackbar.LENGTH_LONG).show();
-        } else {
-            Snackbar.make(this.view, "Bluetooth unavailable on this device.", Snackbar.LENGTH_LONG).show();
-
         }
     }
+
+    public boolean isEnabled() {
+        return bluetoothAdapter.isEnabled();
+    }
+
+    public void enableBluetooth() {
+        if (!isEnabled()) {
+            Intent btOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            activity.startActivityForResult(btOnIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode != Activity.RESULT_OK) {
+                Snackbar.make(view, "Could not enable Bluetooth", Snackbar.LENGTH_LONG).show();
+            } else {
+                Log.v("bluetooth","paired devices");
+                showPairdDevices();
+            }
+        }
+    }
+
+    public void showPairdDevices() {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setMessage("fire missiles?")
+                .setPositiveButton("fire", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        final android.support.v7.app.AlertDialog ad = builder.create();
+        ad.show();
+    }
+
 }
+
--- ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/MainActivity.java
+++ ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/MainActivity.java
@@ -2,14 +2,11 @@ package uk.co.sam.mediacontroller;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import android.bluetooth.BluetoothAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mBluetoothHandler = new BluetoothHandler(this,toolbar);
+        mBluetoothHandler = new BluetoothHandler(this, toolbar);
     }
 
     @Override
@@ -29,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            mBluetoothHandler.onActivityResult(requestCode, resultCode);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -40,14 +45,11 @@ public class MainActivity extends AppCompatActivity {
         switch (id) {
             //noinspection SimplifiableIfStatement
             case R.id.action_connect:
-//                if (mBluetoothAdapter == null) {
-//                    Snackbar.make(this.findViewById(R.id.action_connect), "No Bluetooth available.", Snackbar.LENGTH_LONG).show();
-//                } else {
-//                    if (!mBluetoothAdapter.isEnabled()) {
-//                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-//                    }
-//                }
+                if (mBluetoothHandler.isEnabled()) {
+                    mBluetoothHandler.showPairdDevices();
+                } else {
+                    mBluetoothHandler.enableBluetooth();
+                }
                 return true;
             case R.id.action_disconnect:
                 return true;
