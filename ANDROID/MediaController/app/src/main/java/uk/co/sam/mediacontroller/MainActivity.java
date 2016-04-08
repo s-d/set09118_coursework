729b1eeb92a8f3850dfba09a33007bbc56fbc6d1
Sam <samdixon05@gmail.com>
2016-04-08 12:13:48 +0100

    done som report, son.

--- ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/MainActivity.java
+++ ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/MainActivity.java
@@ -99,7 +99,6 @@ public class MainActivity extends AppCompatActivity {
         mBluetoothHandler = new BluetoothHandler(this, toolbar);
     }
 
-
     public static void hideButtons() {
         for (Button button : mFunctionButtons) {
             button.setVisibility(View.GONE);
