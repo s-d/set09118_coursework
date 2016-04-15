54b0de893bce6c54aede841283d3891765d1bbe9
Sam <samdixon05@gmail.com>
2016-04-15 18:32:31 +0100

    Working on report, got some comments in code as well.

--- ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/BluetoothHandler.java
+++ ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/BluetoothHandler.java
@@ -23,11 +23,10 @@ import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
-/**
- * Created by Sam Dixon on 22/03/2016.
- */
 public class BluetoothHandler {
 
+    private static final UUID MY_UUID = UUID
+            .fromString("00001101-0000-1000-8000-00805f9b34fb");
     private static final int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter mBluetoothAdapter;
     private View mView;
@@ -35,46 +34,56 @@ public class BluetoothHandler {
     private BluetoothDevice mDevice;
     private BluetoothSocket mSocket;
     private OutputStream mOutput;
-    private static final UUID MY_UUID = UUID
-            .fromString("00001101-0000-1000-8000-00805f9b34fb");
     private ProgressDialog progress;
 
 
+    //class constructor
     public BluetoothHandler(Activity activity, View view) {
+        //get activity resources
         this.mView = view;
         this.mActivity = activity;
+        //check mobile device has a Bluetooth adapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
+            //tell user no adapter is available
             Snackbar.make(this.mView, "Bluetooth unavailable on this device.", Snackbar.LENGTH_LONG).show();
-
         }
     }
 
+    //turns on Bluetooth
     public void enableBluetooth() {
+        //check if Bluetooth is already enabled
         if (!isEnabled()) {
+            //start Bluetooth
             Intent btOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            //inform activity that bluetooth has been started
             mActivity.startActivityForResult(btOnIntent, REQUEST_ENABLE_BT);
         } else {
             //do nothing
         }
     }
 
+    //returns whether bluetooth is on or off
     public boolean isEnabled() {
         return mBluetoothAdapter.isEnabled();
     }
 
+    //result method as to whether bluetooth was successfully turned on
     protected void onActivityResult(int requestCode, int resultCode) {
         if (requestCode == REQUEST_ENABLE_BT) {
+            //if bluetooth could not be enabled
             if (resultCode != Activity.RESULT_OK) {
+                //tell user
                 Snackbar.make(mView, "Could not enable Bluetooth", Snackbar.LENGTH_LONG).show();
             } else {
-                showPairedDialog();
+                //show previously paired devices to user
+                showPairedDevicesDialog();
             }
         }
     }
 
-    public void showPairedDialog() {
-        enableBluetooth();
+    //shows a dialog of paired bluetooth devices
+    public void showPairedDevicesDialog() {
         //define various dialog attributes
         final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mActivity);
         final LayoutInflater inflater = mActivity.getLayoutInflater();
@@ -88,19 +97,21 @@ public class BluetoothHandler {
         final ArrayAdapter<String> btArrayAdapter =
                 new ArrayAdapter<>(mActivity, android.R.layout.simple_list_item_1);
 
-        //build dialog
+        //construct the dialog
         dialogBuilder.setTitle("Paired Devices");
         dialogBuilder.setView(dialogLayout);
         pairedListView.setAdapter(btArrayAdapter);
+        //fill dialog with every paired device
         for (BluetoothDevice device : pairedDevices) {
             btArrayAdapter.add(device.getName());
         }
 
-        //dialog cancel button
+        //add cancel button to dialog
         dialogBuilder.setNegativeButton(R.string.bluetooth_handler_paired_dialog,
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
+                        //close the dialog
                         dialog.dismiss();
                     }
                 });
@@ -108,24 +119,22 @@ public class BluetoothHandler {
         //create  dialog
         final AlertDialog ad = dialogBuilder.create();
 
-        //clickable list
+        //set clickListener for every item on the list
         pairedListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String pairedName = ((TextView) view).getText().toString();
-                Log.i("Clicked", pairedName);
                 for (BluetoothDevice device : pairedDevices) {
-                    Log.i("Checking", device.getName());
                     if (pairedName.equals(device.getName())) {
+                        //set selected device
                         mDevice = device;
-                        Log.i("BT Device Set", mDevice.getName());
+                        //dismiss dialog
                         ad.dismiss();
                         try {
+                            //connect to selected device
                             connectDevice();
-                            Log.d("Bluetooth", "attempting connection");
                         } catch (IOException e) {
                             e.printStackTrace();
-                            Log.d("connectDevice", "failure");
                         }
                         break;
                     }
@@ -136,70 +145,87 @@ public class BluetoothHandler {
         ad.show();
     }
 
+    //connect to a selected device
     void connectDevice() throws IOException {
+        //create a progress dialog
         progress = ProgressDialog.show(mActivity, null, "Connecting to " + mDevice.getName(), true);
+        //create new thread for connection process
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
+                    //connect to device
                     mSocket = mDevice.createRfcommSocketToServiceRecord(MY_UUID);
                     mSocket.connect();
+                    //open output stream
                     mOutput = mSocket.getOutputStream();
+                    //dismiss load dialog
                     progress.dismiss();
-                    Snackbar.make(mView, "Connected successful", Snackbar.LENGTH_SHORT).show();
+                    //tell user connection was successful
+                    Snackbar.make(mView, "Connection successful", Snackbar.LENGTH_SHORT).show();
 
+                    //create thread to update UI
                     mActivity.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
+                            //reveal buttons on main page
                             MainActivity.showButtons();
-
                         }
                     });
 
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Log.d("connectDevice", "no worky");
+                    //dismiss load bar
                     progress.dismiss();
-
+                    //inform user connection failed
                     Snackbar snack = Snackbar.make(mView, "Connection failed", Snackbar.LENGTH_LONG);
+                    //add retry button to snack bar
                     snack.setAction("Retry", new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             try {
+                                //try to connect again
                                 connectDevice();
-                            } catch (IOException e1) {
-                                e1.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
                             }
                         }
                     });
+                    //show failed snack bar
                     snack.show();
                 }
             }
+            //start thread
         }).start();
     }
 
+    //disconnect a connected device
     public void disconnectDevice() {
+        //check that the socket
         if (mSocket != null) {
+            //check socket is in use
             if (mSocket.isConnected()) {
                 try {
+                    //close output stream
                     mOutput.close();
+                    //close socket
                     mSocket.close();
+                    //tell user disconnect was successful
                     Snackbar.make(mView, "Disconnected from " + mDevice.getName(), Snackbar.LENGTH_SHORT).show();
+                    //hide buttons on UI
                     MainActivity.hideButtons();
 
                 } catch (IOException e) {
-                    Log.d("disconnectDevice", "failed to disconnect" + e);
                     e.printStackTrace();
-
                 }
             } else {
-                Snackbar.make(mView, "Nothing to disconnect", Snackbar.LENGTH_SHORT).show();
+                //tell user nothing to disconnect
+                Snackbar.make(mView, "Not connected to a device", Snackbar.LENGTH_SHORT).show();
             }
-        } else {
-            Snackbar.make(mView, "Nothing to disconnect", Snackbar.LENGTH_SHORT).show();
         }
     }
 
+    //sends a value to connect device
     public void writeValue(String val) {
         try {
             mOutput.write(val.getBytes());
--- ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/MainActivity.java
+++ ANDROID/MediaController/app/src/main/java/uk/co/sam/mediacontroller/MainActivity.java
@@ -3,10 +3,8 @@ package uk.co.sam.mediacontroller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
@@ -128,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
         buttonConnect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (mBluetoothHandler.isEnabled()) {
-                    mBluetoothHandler.showPairedDialog();
+                    mBluetoothHandler.showPairedDevicesDialog();
                 } else {
                     mBluetoothHandler.enableBluetooth();
                 }
@@ -176,14 +174,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         switch (id) {
             case R.id.action_disconnect:
                     mBluetoothHandler.disconnectDevice();
-            case R.id.action_settings:
         }
         return super.onOptionsItemSelected(item);
     }
