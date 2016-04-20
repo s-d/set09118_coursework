0a68657552ef5a5d9ba5c5ea680b041e7a37d1a6
Sam <samdixon05@gmail.com>
2016-04-20 03:36:47 +0100

    Done loads, Finished implementation sections.

--- ARDUINO/Media/Media.cpp
+++ ARDUINO/Media/Media.cpp
@@ -3,56 +3,53 @@
 #if defined(_USING_HID)
 
 static const uint8_t _hidReportDescriptor[] PROGMEM = {
-  
-    /* Cross-platform support for controls found on IR Medias */
-
-    0x05, 0x0c,                    //	Usage Page (Consumer Devices)
-    0x09, 0x01,                    //	Usage (Consumer Control)
-    0xa1, 0x01,                    //	Collection (Application)
-    0x85, 0x04,                    //	REPORT_ID (4)
-    0x15, 0x00,                    //	Logical Minimum (0)
-    0x25, 0x01,                    //	Logical Maximum (1)
-    0x09, 0xe9,                    //	Usage (Volume Up)
-    0x09, 0xea,                    //	Usage (Volume Down)
-    0x75, 0x01,                    //	Report Size (1)
-    0x95, 0x02,                    //	Report Count (2)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x09, 0xe2,                    //	Usage (Mute)
-    0x95, 0x01,                    //	Report Count (1)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x09, 0xcd,                    //	Usage (Play)
-    0x95, 0x01,                    //	Report Count (1)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x09, 0xb1,                    //	Usage (Pause)
-    0x95, 0x01,                    //	Report Count (1)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x09, 0xb7,                    //	Usage (Stop)
-    0x95, 0x01,                    //	Report Count (1)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x09, 0xb5,                    //	Usage (Next)
-    0x95, 0x01,                    //	Report Count (1)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x09, 0xb6,                    //	Usage (Previous)
-    0x95, 0x01,                    //	Report Count (1)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x09, 0xb3,                    //	Usage (Fast Forward)
-    0x95, 0x01,                    //	Report Count (1)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x09, 0xb4,                    //	Usage (Rewind)
-    0x95, 0x01,                    //	Report Count (1)
-    0x81, 0x06,                    //	Input (Data, Variable, Relative)
-
-    0x95, 0x06,                    //	Report Count (6) Number of bits remaining in byte
-    0x81, 0x07,                    //	Input (Constant, Variable, Relative) 
-    0xc0                           //	End Collection
+    0x09, 0x01,		//Usage (Consumer Control)
+    0x05, 0x0c,		//Usage Page (Consumer Devices)
+    0xa1, 0x01,		//Collection (Application)
+    0x85, 0x04,		//REPORT_ID (4)
+    0x15, 0x00,		//Logical Minimum (0)
+    0x25, 0x01,		//Logical Maximum (1)
+    0x09, 0xe9,		//Usage (Volume Up)
+    0x09, 0xea,		//Usage (Volume Down)
+    0x75, 0x01,		//Report Size (1)
+    0x95, 0x02,		//Report Count (2)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x09, 0xe2,		//Usage (Mute)
+    0x95, 0x01,		//Report Count (1)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x09, 0xcd,		//Usage (Play)
+    0x95, 0x01,		//Report Count (1)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x09, 0xb1,		//Usage (Pause)
+    0x95, 0x01,		//Report Count (1)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x09, 0xb7,		//Usage (Stop)
+    0x95, 0x01,		//Report Count (1)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x09, 0xb5,		//Usage (Next)
+    0x95, 0x01,		//Report Count (1)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x09, 0xb6,		//Usage (Previous)
+    0x95, 0x01,		//Report Count (1)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x09, 0xb3,		//Usage (Fast Forward)
+    0x95, 0x01,		//Report Count (1)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x09, 0xb4,		//Usage (Rewind)
+    0x95, 0x01,		//Report Count (1)
+    0x81, 0x06,		//Input (Data, Variable, Relative)
+		
+    0x95, 0x06,		//Report Count (6) Number of bits remaining in byte
+    0x81, 0x07,		//Input (Constant, Variable, Relative) 
+    0xc0       		//End Collection
 };
  
 Media_::Media_(void)
